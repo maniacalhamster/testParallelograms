dbdaeeda3ee20dc7d42021e547a62d2b8f3987ff
Akar Singh <aks002@ucsd.edu>
2020-02-21 08:12:19 -0800

    Brushed up on some commentary

--- testAll.sh
+++ testAll.sh
@@ -3,18 +3,21 @@
 # Automates the testing of all possible input for parallelograms.sh
 
 # variables used within script
+# INPUT used to handle all user input prompts to script
+# YES used to build the repetitive portions of INPUT
 INPUT=""
 YES="\ny\n"
 
 # loop for generating the repetitive portions of the INPUT
+# 'number (2..19) newline y(es) newline' repeatedly added onto INPUT
 for inp in {2..20..1}; do
 	INPUT=$INPUT$inp$YES
 done
-# final case will reject a replay
+# final case '20 newline n(o) newline' will reject a replay
 INPUT=$INPUT"20\nn\n"
 
-# pipes the output of INPUT to both student and public scripts, redirecting
-# both output to files later used to check differences
+# pipes the content of INPUT to both student and public scripts, redirecting
+# both output to respective output files later used to check differences
 printf $INPUT | ./parallelograms.sh > myOutput
 echo "Student script done - saved to 'myOutput'"
 printf $INPUT | parallelograms > publicOutput
